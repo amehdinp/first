c39db6e489eafa9a08a2c26eaede9dbd5285355a
amehdinp <mehdinourmandi@gmail.com>
2021-10-22 16:49:08 +0330

    creat html and read

--- articles/views.py
+++ articles/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import models
 # Create your views here.
 
 
+
 def articles_list(request):
-    return HttpResponse("articles_list")
+    articles1 = models.Article.objects.all()
+    arg={'articles':articles1}
+    return render(request, 'articles/articles_list.html',arg)
 
 def creat_article(request):
-    return HttpResponse("creat_article")
+    return render(request,'articles/creat_article.html')
+
 
 
 def details(request):
-    return HttpResponse("details")
+    return render(request,'articles/article_detail.html')
